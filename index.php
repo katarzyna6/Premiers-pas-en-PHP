fa60853abd635d54ab378f1cdbc4662954dc504d
katarzyna <katharina.r@hotmail.fr>
2020-03-09 14:51:48 +0100

    ex.2 ordre des nombres

--- index.php
+++ index.php
@@ -4,11 +4,14 @@ var_dump($_GET);
 
 $nb1 = $_GET['nb1'];
 $nb2 = $_GET['nb2'];
+$nb3 = $_GET['nb3'];
 
-if(($nb1 < 0 && $nb2 < 0) || ($nb1 > 0 && $nb2 > 0)) {
-    $result = "positif";
-} else{
-    $result = "négatif";
+if(($nb1 < $nb2 && $nb2 < $nb3)) {
+    $ordre = "croissant";
+} else if(($nb1 > $nb2 && $nb2 > $nb3)){
+    $ordre = "décroissant";
+} else {
+    $ordre = "particulier";
 }
 
 ?>
@@ -21,7 +24,7 @@ if(($nb1 < 0 && $nb2 < 0) || ($nb1 > 0 && $nb2 > 0)) {
 </head>
 <body>
 <h1>Bonjour</h1>
-<p>Le résultat sera <?= $result ?> </p>
+<p>L'ordre sera <?= $ordre ?> </p>
 
 </body>
 </html> 
